a11167a5c06f58a69dd2b02527cdf6e97f9842e3
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-05-26 18:39:08 +0530

    Update Day-19(Search in a sorted rotated array).cpp

--- Day-19(Search in a sorted rotated array).cpp
+++ Day-19(Search in a sorted rotated array).cpp
@@ -1,3 +1,20 @@
+/*
+Time complexity   : O(log n)
+Space complexity  : O(1)
+My approach : Use of Modified Binary Search.
+Algorithm:
+1.Find middle point mid = (l + h)/2
+2.If key is present at middle point, return mid.
+3.Else If arr[l..mid] is sorted
+    a) If key to be searched lies in range from arr[l]
+       to arr[mid], recur for arr[l..mid].
+     b) Else recur for arr[mid+1..h]
+4.Else (arr[mid+1..h] must be sorted)
+   a) If key to be searched lies in range from arr[mid+1]
+    to arr[h], recur for arr[mid+1..h].
+    b) Else recur for arr[l..mid]
+*/
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
